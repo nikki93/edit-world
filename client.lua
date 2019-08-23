2732f96b4ef918ab461c7be8dc31bd0adae59611
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2019-08-22 18:44:23 -0700

    center camera around player when teleporting

--- client.lua
+++ client.lua
@@ -299,6 +299,7 @@ function client.update(dt)
                                 theTransform:rotate(target.rotation)
                                 local tx, ty = theTransform:transformPoint(0.5 * target.width, 0.5 * target.height)
                                 home.x, home.y = tx - 0.5 * G, ty - 0.5 * G
+                                cameraX, cameraY = tx - love.graphics.getWidth(), ty - love.graphics.getHeight()
                             end
                         end
                     end
