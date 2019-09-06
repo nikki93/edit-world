ffa09a2c4893d9219c022abf81a411bd05e447d4
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2019-09-05 18:13:50 -0700

    fix for remote server

--- common/node_image.lua
+++ common/node_image.lua
@@ -20,7 +20,7 @@ node_image.proxyMethods = setmetatable({}, { __index = node_base.proxyMethods })
 node_image.proxyMetatable = { __index = node_image.proxyMethods }
 
 
-if not castle.system.isRemoteServer() then
+if CASTLE_SERVER then
     local defaultImage = love.graphics.newImage('assets/checkerboard.png')
 
     local imageCache = {}
