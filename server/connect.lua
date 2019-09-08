eaae22c8596ef0d66914380dcca373adc4ad388e
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2019-09-08 15:32:58 -0700

    start players in quantized positions

--- server/connect.lua
+++ server/connect.lua
@@ -7,5 +7,5 @@ local share = server.share
 function server.connect(clientId)
     share.players[clientId] = {}
     local player = share.players[clientId]
-    player.x, player.y = 4 * (2 * math.random() - 1), 4 * (2 * math.random() - 1)
+    player.x, player.y = math.random(-4, 4), math.random(-4, 4)
 end
