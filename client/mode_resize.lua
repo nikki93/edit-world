aee7f32ba13e36f3ef162cfcfcddaf9a431776ee
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2019-09-08 18:22:10 -0700

    half-fix multi-resize logic

--- client/mode_resize.lua
+++ client/mode_resize.lua
@@ -6,7 +6,7 @@ local camera = require 'client.camera'
 local mode_resize = {}
 
 
-local nSelected
+local nSelections
 local worldPivotX, worldPivotY = 0, 0
 
 
@@ -15,14 +15,14 @@ local worldPivotX, worldPivotY = 0, 0
 --
 
 function mode_resize.update(dt)
-    nSelected = selections.numSelections('primary')
+    nSelections = selections.numSelections('primary')
 
     worldPivotX, worldPivotY = 0, 0
     selections.forEach('primary', function(id, node)
         local worldX, worldY = space.getWorldSpace(node).transform:transformPoint(0, 0)
         worldPivotX, worldPivotY = worldPivotX + worldX, worldPivotY + worldY
     end)
-    worldPivotX, worldPivotY = worldPivotX / nSelected, worldPivotY / nSelected
+    worldPivotX, worldPivotY = worldPivotX / nSelections, worldPivotY / nSelections
 end
 
 
@@ -36,49 +36,37 @@ function mode_resize.mousemoved(screenMouseX, screenMouseY, screenMouseDX, scree
         local prevScreenMouseX, prevScreenMouseY = screenMouseX - screenMouseDX, screenMouseY - screenMouseDY
         local prevWorldMouseX, prevWorldMouseY = camera.getTransform():inverseTransformPoint(prevScreenMouseX, prevScreenMouseY)
 
-        if nSelected == 1 then -- Scale single node around its own origin
-            selections.forEach('primary', function(id, node)
-                local transform = space.getWorldSpace(node).transform
-                local prevLocalMouseX, prevLocalMouseY = transform:inverseTransformPoint(prevWorldMouseX, prevWorldMouseY)
-                local localMouseX, localMouseY = transform:inverseTransformPoint(worldMouseX, worldMouseY)
-                if math.abs(prevLocalMouseX) >= 0.5 and math.abs(localMouseX) >= 0.5 then
-                    node.width = node.width * localMouseX / prevLocalMouseX
-                end
-                if math.abs(prevLocalMouseY) >= 0.5 and math.abs(localMouseY) >= 0.5 then
-                    node.height = node.height * localMouseY / prevLocalMouseY
-                end
-            end)
-        elseif nSelected > 1 then -- Scale multiple nodes around the centroid of their origins
-            -- Compute scale factor
-            local prevPivotMouseX, prevPivotMouseY = prevWorldMouseX - worldPivotX, prevWorldMouseY - worldPivotY
-            local pivotMouseX, pivotMouseY = worldMouseX - worldPivotX, worldMouseY - worldPivotY
-            local scaleX, scaleY = 1, 1
-            if math.abs(prevPivotMouseX) >= 0.5 and math.abs(pivotMouseX) >= 0.5 then
-                scaleX = pivotMouseX / prevPivotMouseX
-            end
-            if math.abs(prevPivotMouseY) >= 0.5 and math.abs(pivotMouseY) >= 0.5 then
-                scaleY = pivotMouseY / prevPivotMouseY
-            end
+        -- Actually scale nodes
+        selections.forEach('primary', function(id, node)
+            -- Update size by checking scaling of mouse position around pivot in local space
+            local transform = space.getWorldSpace(node).transform
+            local pivotLX, pivotLY = transform:inverseTransformPoint(worldPivotX, worldPivotY)
+            local prevMouseLX, prevMouseLY = transform:inverseTransformPoint(prevWorldMouseX, prevWorldMouseY)
+            local mouseLX, mouseLY = transform:inverseTransformPoint(worldMouseX, worldMouseY)
+            local prevDLX, prevDLY = prevMouseLX - pivotLX, prevMouseLY - pivotLY
+            local dLX, dLY = mouseLX - pivotLX, mouseLY - pivotLY
+            node.width = node.width * dLX / prevDLX
+            node.height = node.height * dLY / prevDLY
 
-            -- Actually scale nodes
-            selections.forEach('primary', function(id, node)
-                -- Update size
-                node.width, node.height = node.width * scaleX, node.height * scaleY
-
-                -- Update position by scaling pivot->node delta vector in parent-space
-                local parentWorldTransform = space.getParentWorldSpace(node).transform
-                local parentPivotX, parentPivotY = parentWorldTransform:inverseTransformPoint(worldPivotX, worldPivotY)
-                local dX, dY = node.x - parentPivotX, node.y - parentPivotY
-                dX, dY = dX * scaleX, dY * scaleY
-                node.x, node.y = dX + parentPivotX, dY + parentPivotY
-            end)
-        end
+            if nSelections > 1 then
+                -- Update position by checking scaling of mouse position around pivot in parent space
+                -- TODO(nikki): This isn't really working...
+                -- local transform = space.getParentWorldSpace(node).transform
+                -- local pivotLX, pivotLY = transform:inverseTransformPoint(worldPivotX, worldPivotY)
+                -- local prevMouseLX, prevMouseLY = transform:inverseTransformPoint(prevWorldMouseX, prevWorldMouseY)
+                -- local mouseLX, mouseLY = transform:inverseTransformPoint(worldMouseX, worldMouseY)
+                -- local prevDLX, prevDLY = prevMouseLX - pivotLX, prevMouseLY - pivotLY
+                -- local dLX, dLY = mouseLX - pivotLX, mouseLY - pivotLY
+                -- node.x = pivotLX + (node.x - pivotLX) * dLX / prevDLX
+                -- node.y = pivotLY + (node.y - pivotLY) * dLY / prevDLY
+            end
+        end)
     end
 end
 
 function mode_resize.getCursorName()
     -- Pick cursor orientation based on the quadrant it is in relative to the pivot
-    if nSelected == 0 then
+    if nSelections == 0 then
         return 'size_se'
     end
     local worldMouseX, worldMouseY = camera.getTransform():inverseTransformPoint(love.mouse.getPosition())
