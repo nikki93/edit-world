12b79f219ea0de5c17ed36e5ad977f2ce8e633f4
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2019-09-10 16:30:40 -0700

    ui for image nodes

--- common/node_image.lua
+++ common/node_image.lua
@@ -1,4 +1,6 @@
 local node_base = require 'common.node_base'
+local ui_utils = require 'common.ui_utils'
+local ui = castle.ui
 
 
 local node_image = {}
@@ -84,6 +86,94 @@ if not CASTLE_SERVER then
     end
 else
     function node_image.proxyMethods:draw(transform)
+    end
+end
+
+
+--
+-- UI
+--
+
+local sectionOpen = true
+
+function node_image.proxyMethods:ui(props)
+    node_base.proxyMethods.ui(self, props)
+
+    local node, nodeManager = self.__node, self.__nodeManager
+
+    local function inside()
+        -- URL
+        ui.textInput('url', node.image.url, {
+            onChange = props.validateChange(function(newUrl)
+                node.image.url = newUrl
+            end),
+        })
+
+        -- Color
+        local color = node.image.color
+        ui.colorPicker('color', color.r, color.g, color.b, color.a, {
+            onChange = props.validateChange(function(newColor)
+                color.r, color.g, color.b, color.a = newColor.r, newColor.g, newColor.b, newColor.a
+            end),
+        })
+
+        -- Smooth scaling, crop
+        ui_utils.row('smooth-scaling-crop', function()
+            ui.toggle('smooth scaling', 'smooth scaling', node.image.smoothScaling, {
+                onToggle = props.validateChange(function(newSmoothScaling)
+                    node.image.smoothScaling = newSmoothScaling
+                end),
+            })
+        end, function()
+            ui.toggle('crop', 'crop', node.image.crop, {
+                onToggle = props.validateChange(function(newCrop)
+                    node.image.crop = newCrop
+                end),
+            })
+        end)
+        if node.image.crop then
+            ui_utils.row('crop-xy', function()
+                ui.numberInput('crop x', node.image.cropX, {
+                    onChange = props.validateChange(function(newCropX)
+                        node.image.cropX = newCropX
+                    end),
+                })
+            end, function()
+                ui.numberInput('crop y', node.image.cropY, {
+                    onChange = props.validateChange(function(newCropY)
+                        node.image.cropY = newCropY
+                    end),
+                })
+            end)
+            ui_utils.row('crop-size', function()
+                ui.numberInput('crop width', node.image.cropWidth, {
+                    onChange = props.validateChange(function(newCropWidth)
+                        node.image.cropWidth = newCropWidth
+                    end),
+                })
+            end, function()
+                ui.numberInput('crop height', node.image.cropHeight, {
+                    onChange = props.validateChange(function(newCropHeight)
+                        node.image.cropHeight = newCropHeight
+                    end),
+                })
+            end)
+            if ui.button('reset crop') then
+                props.validateChange(function()
+                    local image = node_image.imageFromUrl(node.image.url)
+                    if image then
+                        node.image.cropX, node.image.cropY = 0, 0
+                        node.image.cropWidth, node.image.cropHeight = image:getDimensions()
+                    end
+                end)()
+            end
+        end
+    end
+
+    if props.surroundSection ~= false then
+        sectionOpen = ui.section('image', { open = sectionOpen }, inside)
+    else
+        inside()
     end
 end
 
