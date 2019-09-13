a58d1d265583a7c379fcd4869508b0674917adba
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2019-09-13 10:09:06 -0700

    use my fork of sfxr with sawtooth wave fix

--- common/lib.lua
+++ common/lib.lua
@@ -8,7 +8,7 @@ lib.state = require 'https://raw.githubusercontent.com/castle-games/share.lua/62
 lib.uuid = require 'https://raw.githubusercontent.com/Tieske/uuid/75f84281f4c45838f59fc2c6f893fa20e32389b6/src/uuid.lua'
 lib.uuid.seed()
 
-lib.sfxr = require 'https://raw.githubusercontent.com/nucular/sfxrlua/27511554ab63b834a8d8b34437c4ba5f0f589fdf/sfxr.lua'
+lib.sfxr = require 'https://raw.githubusercontent.com/nikki93/sfxrlua/b7377e848fdf6b86811ed28deedeafc50fa16ca0/sfxr.lua'
 
 lib.serpent = require 'https://raw.githubusercontent.com/pkulchenko/serpent/879580fb21933f63eb23ece7d60ba2349a8d2848/src/serpent.lua'
 
