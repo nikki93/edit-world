50e9745af0fb878d33751ddbab2b51b78c6ca016
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2019-09-15 19:14:16 -0700

    make a new node on connect on mobile

--- client/connect.lua
+++ client/connect.lua
@@ -2,6 +2,7 @@ local client = require 'client.init'
 local locals = require 'client.locals'
 local node_manager = require 'common.node_manager'
 local player = require 'client.player'
+local mode_select = require 'client.mode_select'
 
 
 local share = client.share
@@ -38,5 +39,9 @@ function client.connect()
     -- Loaded
     network.async(function()
         locals.loaded = true
+
+        if not castle.system.isDestkop() then
+            mode_select.newNode()
+        end
     end)
 end
