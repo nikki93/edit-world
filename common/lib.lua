b6a2960834296180509726d81bacf405a3adbbc0
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2019-10-03 19:15:12 +0530

    update 'share.lua'

--- common/lib.lua
+++ common/lib.lua
@@ -1,9 +1,9 @@
 local lib = {}
 
 
-lib.cs = require 'https://raw.githubusercontent.com/castle-games/share.lua/623c500de3cbfd544db9e6f2bdcd03b5b7e6f377/cs.lua'
+lib.cs = require 'https://raw.githubusercontent.com/castle-games/share.lua/949bd274ce6ed5acf1fa30331c1af97b7e6052d7/cs.lua'
 
-lib.state = require 'https://raw.githubusercontent.com/castle-games/share.lua/623c500de3cbfd544db9e6f2bdcd03b5b7e6f377/state.lua'
+lib.state = require 'https://raw.githubusercontent.com/castle-games/share.lua/949bd274ce6ed5acf1fa30331c1af97b7e6052d7/state.lua'
 
 lib.uuid = require 'https://raw.githubusercontent.com/Tieske/uuid/75f84281f4c45838f59fc2c6f893fa20e32389b6/src/uuid.lua'
 lib.uuid.seed()
